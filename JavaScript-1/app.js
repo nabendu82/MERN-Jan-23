c1ba521f79f7af9252385800455de848da779647
Nabendu <nabendu.biswas@gmail.com>
2023-02-04 06:10:55 +0530

    JavaScript-1

---
+++ JavaScript-1/app.js
@@ -0,0 +1,64 @@
+// console.log(+8)
+
+//js - html css
+//js - basic -- Dom
+
+//Varibale- container to store data.
+
+//Declaration 
+//keyword -  identifier - value.
+
+//keyword - reserved words
+//Var - Let , const
+
+//idetifier - name to your container\
+
+//value - datatypes
+
+
+
+
+
+// let firstName = 'John';
+// firstName = 'Johan'
+
+// console.log(firstName)
+
+// datatypes
+
+//primitive datatypes
+   //string,number,boolean,null,undefined, - Bigint - symbols
+//reference datatypes:
+    // Object - Array
+
+    //string - '' ,"",`` -strings
+    //typeof - return the datatype
+
+    // // let str = "6777654"
+    // let str = 0
+
+
+    // console.log(typeof str)
+
+    //boolean - true or false;
+
+    // let str = 6<8
+    // console.log(typeof str)
+
+    // let str = null;
+
+    // console.log(typeof str);
+
+    // let srt1 ;
+    // console.log(typeof str1)
+
+
+
+  
+
+
+
+
+
+
+
