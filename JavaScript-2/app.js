42288fa4e6315b8c2e9b9acfbcd713209456f109
Nabendu <nabendu.biswas@gmail.com>
2023-02-07 17:09:53 +0530

    Add files via upload

---
+++ JavaScript-2/app.js
@@ -0,0 +1,153 @@
+// console.log(+8)
+
+//js - html css
+//js - basic -- Dom
+
+//Varibale- container to store data.
+
+//Declaration 
+//keyword -  identifier - value.
+
+//keyword - reserved words
+//Var - Let , const
+
+//idetifier - name to your container\
+
+//value - datatypes
+
+
+
+
+
+// let firstName = 'John';
+// firstName = 'Johan'
+
+// console.log(firstName)
+
+// datatypes
+
+//primitive datatypes
+   //string,number,boolean,null,undefined, - Bigint - symbols
+//reference datatypes:
+    // Object - Array
+
+    //string - '' ,"",`` -strings
+    //typeof - return the datatype
+
+    // // let str = "6777654"
+    // let str = 0
+
+
+    // console.log(typeof str)
+
+    //boolean - true or false;
+
+    // let str = 6<8
+    // console.log(typeof str)
+
+    // let str = null;
+
+    // console.log(typeof str);
+
+    // let srt1 ;
+    // console.log(typeof str1)
+
+    //refernece datatypes:
+
+   //  let arr = [1,2,3,34,4,,34,4,'helo world',undefined,null,true,6];
+
+   //  console.log(arr)
+   //  console.log(arr[0])
+
+   //  console.log(typeof arr)
+
+    //object - 
+
+   //  const car = {
+   //    //key - value - property;
+   //    name : 'Audi',
+   //    price:231111112213,
+   //  }
+
+   //  console.log(car.price)
+
+   //  const person1= {
+   //    //data
+   //    name:'John',
+   //    age:45,
+   //    address:
+   //  }
+
+
+   // const c = Symbol('foo');
+   // const d = Symbol('foo');
+
+   // console.log(c === d );
+
+
+   //function
+
+   // 1 declaration
+
+   // function - block statement
+
+   // let a1 = 45556;
+   // let b1 = 7676;
+
+   // function add(a,b){
+   //    console.log(a,b)
+   //    let sum = a+b;
+   //    return sum;
+   // }
+
+   // console.log(add(a1,b1))
+
+
+   //obj - Data - Behaviour together.
+
+   // const obj = {
+   //    //key : value - property data
+   //    name:'foo',
+   //    age:24,
+
+   //    //methods
+   //    details:function(){
+   //       return obj.name+ obj.age;
+   //    }
+   // }
+
+
+// const arrr = [12,3,21,2321]
+// console.warn(arrr)
+// obj.details()
+// const str = 'hjsdahjhsad';
+// console.log(str.toUpperCase())
+
+//W3schools - MDN DOCS
+
+
+
+   
+
+   
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
