3606de3eee48a14066636c52d37fb463520fba7b
Nabendu <nabendu.biswas@gmail.com>
2023-02-10 09:40:09 +0530

    Add files via upload

---
+++ JavaScript-4/day3.js
@@ -0,0 +1,60 @@
+//loop -
+
+//for - while - do while
+
+// console.log('Helo');
+
+
+// for(let i = 0; i<100; i++ ){
+//     console.log('Hello')
+// }
+
+// let i = 100;
+
+// while(i<100){
+//    console.log('hello');
+//    i++;
+// }
+
+// let i = 100;
+
+// do{
+//  console.log('hello');
+//  i++;
+
+// }while(i<100)
+
+//iterations loop
+
+// const a = [1,2,,2,3,7,43,4,34,3];
+
+//iterantion - 
+
+// console.log(a.length)
+
+// for (let i = 0; i< a.length;i++){
+//     console.log(a[i] * 2)
+// }
+
+//forEach - array iteration
+// a.forEach(function(value, index, array ){
+//  console.log(value * 2)
+// }) 
+
+
+// Dom
+
+const head= document.getElementById('head')
+const hh = document.getElementsByTagName('h1')
+
+console.log(hh);
+
+//innerHTML;
+// head.innerHTML='<em>Subscribe</em>'
+
+head.style.color='orange'
+
+
+
+
+
