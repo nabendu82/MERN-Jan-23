fe472bb53c7d51284e159715b7de7090783c5b4b
Nabendu <nabendu.biswas@gmail.com>
2023-02-15 12:05:50 +0530

    Add files via upload

---
+++ JavaScript-6/day5.js
@@ -0,0 +1,55 @@
+// selector - 
+//id
+
+//class - multiple selctor
+
+// const head = document.getElementsByClassName('info')
+
+// console.log(head)
+
+// head[1].style.color = 'red'
+
+
+// const head = document.getElementsByTagName('h1')
+
+// console.log(head)
+
+// head[2].style.color = 'red'
+
+//2 selector - 
+
+// const head = [1,2,2,312,12]
+
+// head.forEach(function(value){
+//     console.log(value)
+// })
+
+// document.querySelector  - select only first coming 
+// document.querySelectorAll
+// all the
+
+// const head = document.querySelectorAll('.info')
+// console.log(head)
+
+
+// head.forEach(function(value){
+//     console.log(value)
+// })
+
+//navi - nodes
+
+
+// const nooode = document.querySelector('.info')
+
+// const para = document.createElement('p')
+
+// const node = document.createTextNode("This is a new paragraph.");
+
+
+// para.appendChild(node)
+
+// nooode.appendChild(para)
+// console.log()
+
+
+// nooode.remove()
