86eb01436c935b69171b3959089e1d2724b5f86e
Nabendu <nabendu.biswas@gmail.com>
2023-02-21 07:56:55 +0530

    Add files via upload

---
+++ JavaScript-8/day2.js
@@ -0,0 +1,104 @@
+//Methods - loops conditional
+
+// const str = 'hello  woorld';
+
+// console.log(str.substr(7,2))
+
+
+// MATH 
+
+// const num = 23;
+// const str = num.toString()
+// console.log(typeof num)
+
+
+//Array - four 
+
+// const arr = ['apple','orange','kiwi'];
+
+//pop
+// console.log(arr);
+
+// arr.pop();
+// arr.push(2)
+// arr.shift()
+// arr.unshift(1)
+
+// console.log(arr)
+
+
+// condition statemnet
+
+// const coin = 'HEd' ;
+
+// // if else elseif switch;
+
+// if(coin.toLowerCase() === 'head'){
+//     console.log('Exc')
+// }else{
+//     console.log('no')
+// }
+
+// let a = 10;
+// let b = 10;
+
+//  a -=b;
+// console.log(a)
+
+// console.log(coin < 1 || coin >=1)
+
+
+//traffic light  -
+//  red - stop
+//  green - go
+//yellow - ready
+//-tme
+
+// let color = 'green';
+
+// if(color ==='red'){
+//    console.log('Stop')
+// }else if(color ==='green'){
+//   console.log('Go')
+// }else if (color === 'yellow'){
+//     console.log('ready')
+// }else{
+//     console.log('TME')
+// }
+
+// switch(color){
+
+//     case 'red':
+//         console.log('Stop');
+//         break;
+//     case 'green':
+//         console.log('go');
+//         break;
+//     case 'yellow':
+//         console.log('ready');
+//         break;
+//     default:
+//         console.log('TME')
+// }
+
+//loops - fro, while, do while
+
+// console.log('JS')
+
+
+// for(i = 0 ; i < 1000 ; i++){
+//     console.log('JS')
+
+// }
+
+//selwct - paro - value
+
+
+//id - uniques
+
+
+
+
+
+
+
---
+++ JavaScript-8/day3.js
@@ -0,0 +1,50 @@
+//loop -
+
+//for - while - do while
+
+// console.log('Helo');
+
+
+// for(let i = 0; i<100; i++ ){
+//     console.log('Hello')
+// }
+
+// let i = 100;
+
+// while(i<100){
+//    console.log('hello');
+//    i++;
+// }
+
+// let i = 100;
+
+// do{
+//  console.log('hello');
+//  i++;
+
+// }while(i<100)
+
+//iterations loop
+
+// const a = [1,2,,2,3,7,43,4,34,3];
+
+//iterantion - 
+
+// console.log(a.length)
+
+// for (let i = 0; i< a.length;i++){
+//     console.log(a[i] * 2)
+// }
+
+//forEach - array iteration
+// a.forEach(function(value, index, array ){
+//  console.log(value * 2)
+// }) 
+
+
+
+
+
+
+
+
