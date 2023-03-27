a66192075bb8b45eb4867d482ee38ed25efd2c70
Nabendu <nabendu.biswas@gmail.com>
2023-03-27 21:29:48 +0530

    Add files via upload

---
+++ my-app-3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserGreeting from './components/UserGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <UserGreeting />
+    </div>
+  );
+}
+
+export default App;
