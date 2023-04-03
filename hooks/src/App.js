d15a3ee603a1f18411d3da34eee791c0480b6450
Nabendu <nabendu.biswas@gmail.com>
2023-04-03 21:13:24 +0530

    Add files via upload

---
+++ hooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HookObject from './components/HookObject';
+
+function App() {
+  return (
+    <div className="App">
+      <HookObject />
+    </div>
+  );
+}
+
+export default App;
