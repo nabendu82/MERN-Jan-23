670c67c33defba40d3fda31c22b19f646c14be07
Nabendu <nabendu.biswas@gmail.com>
2023-04-13 21:30:31 +0530

    Add files via upload

---
+++ contact-app-2/src/components/ContactList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ContactCard from './ContactCard'
+import { Link } from 'react-router-dom'
+
+const ContactList = props => {
+  return (
+    <div className="main">
+      <h2>Contact List</h2>
+      <Link to="/add">
+        <button className='ui violet button'>Add Contact</button>
+      </Link>
+      <div className='ui celled list'>
+        {props.contacts.map(contact => <ContactCard key={contact.id} contact={contact} clickHandler={ id => props.getContactID(id) } />)}
+      </div>
+    </div>
+  )
+}
+
+export default ContactList
